95f7c2e3d839b555dcd6f0322ef78596da9d2aab
bharatbhangale <contact.bharat.cse@gmail.com>
2024-04-21 00:11:39 +0530

    middleware changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/login(.*)", "/app(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/app"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
