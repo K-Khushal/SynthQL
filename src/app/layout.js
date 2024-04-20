82eb7632ccae394d997727bb19736ef8bc0b6c1f
Khushal <khandelwalkhushal79@gmail.com>
2024-04-20 17:50:32 +0530

    added navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {Providers} from "@/app/Providers";
+import Navigation from "@/components/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-[#121212] text-white`}>{children}</body>
+    <html lang="en" className="bg-[#1C  ]">
+      <body className={`${inter.className} bg-[#121212] text-white dark`}>
+      <Providers>
+        <Navigation/>
+      {children}
+      </Providers>
+      </body>
     </html>
   );
 }
