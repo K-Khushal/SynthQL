b01fca74717ac0af7cd143fbe1326907c9b17693
Khushal <khandelwalkhushal79@gmail.com>
2024-04-20 15:09:42 +0530

    added hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <HeroSection/>
+    </>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+
+
+export default function HeroSection() {
+    return (
+        <section className="bg-[#121212] text-white">
+        <div className="container mx-auto">
+            <h1 className="text-4xl font-bold">SynthQL</h1>
+            <p className="text-lg">An AI SQL Query builder</p>
+        </div>
+        </section>
+    );
+}
