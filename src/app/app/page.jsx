061b873befa6cf84ea29da8bfd331f784ff2d665
Khushal Khandelwal <128228211+K-Khushal@users.noreply.github.com>
2024-04-21 02:00:04 +0530

    added text2sql

--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -1,13 +1,16 @@
 import HeroSection from "@/components/HeroSection";
 import SideNavbar from "@/components/SideNavbar";
+import SQLBuilder from "@/components/SQLBuilder";
 
 export default function App() {
   return (
     <>
         <main>
-            <div className="h-screen">
+            <div className="h-screen flex flex-row">
                 <SideNavbar/>
-
+                <div className="p-12">
+                    <SQLBuilder/>
+                </div>
             </div>
         </main>
     </>
